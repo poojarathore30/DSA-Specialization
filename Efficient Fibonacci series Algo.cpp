20ad59cc32a2aca1baa79d5fa7926b53c1b29112
Pooja Rathore <poojarathore.nks@gmail.com>
2020-07-24 22:32:27 +0530

    Fibonnaci series efficient algo

---
+++ Efficient Fibonacci series Algo.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<long> fib(int n)
+{
+	int f=0;
+	int s=1;
+vector<long> v;
+v.push_back(f);
+v.push_back(s);
+	for(int i=2;i<=n;i++)
+	{
+		v.push_back(v[i-1]+v[i-2]);
+	}
+	return v;
+}
+int main()
+{
+	cout<<"enter the number for calc Fibonacci series :"<<endl;
+	int n;
+	cin>>n;
+	vector<long> v=fib(n);
+	cout<<"Fib series is :"<<endl;
+    for(int i=0;i<n;i++)
+    cout<<v[i]<<" ";
+
+	return 0;
+}
